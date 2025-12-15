9fcfc9e8a15462aa3386d856773d03abe5d085a3
Noah van Dongen <nnnvandongen@gmail.com>
2025-12-15 21:06:46 +0100

    Implement authentication handler with headers
    Added a handler function to manage authentication requests with CORS and cache headers.

--- api/auth.ts
+++ api/auth.ts
@@ -1,1 +1,9 @@
-export { auth as default } from "@openlab/vercel-netlify-cms-github";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { auth } from "@openlab/vercel-netlify-cms-github";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.setHeader("Cache-Control", "no-store");
+
+  return auth(req, res);
+}
