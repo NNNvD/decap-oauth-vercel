a9517644c6219d00aaf7d5decb11951ace0a32f4
Noah van Dongen <nnnvandongen@gmail.com>
2025-12-15 21:41:50 +0100

    Refine comments in callback handler

--- api/callback.ts
+++ api/callback.ts
@@ -2,10 +2,8 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { callback } from "@openlab/vercel-netlify-cms-github";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // Preserve window.opener for cross-origin OAuth popups
+  // Preserve window.opener for OAuth popups
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-
-  // (Optional but harmless) prevent caching weirdness
   res.setHeader("Cache-Control", "no-store");
 
   return callback(req, res);
